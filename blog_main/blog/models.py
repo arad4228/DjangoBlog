3b7f215f5c631a7ce797be526de4e41ef6d82411
arad4228 <arad4228@gmail.com>
2022-04-08 15:34:11 +0900

    Post에 Author 필드 제작

--- blog_main/blog/models.py
+++ blog_main/blog/models.py
@@ -1,8 +1,12 @@
 import os.path
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30) # 제목
     hook_msg = models.TextField(blank=True)           # Hook 메시지
@@ -13,15 +17,18 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)  # 만든 날짜: auto_now_add는 최초 생성시에만 적용된다.
     updated_at = models.DateTimeField(auto_now=True)      # auto_now는 업데이트 될 때마다 적용된다.
-    # author: 추후 작성 예정
 
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)      # Call-Back 함수를 Set.(SET_NULL, ..)
+                                                                                # Front,DB에서 값을 넣을 때 2번 확인한다.
+                                                                                # null=True는 DB에 해당한다.
+                                                                                # Front에서 확인 하는 경우는 Blank
 
     #method
     def __str__(self):
         # f는 formated string의 형태이고 이는 python의 기능이다.
         # ' '은 내부의 내용을 알맞은 format에 맞춰 데이터를 가저온다.
         # { }안에 변수를 넣으면, 해당 변수의 값이 나온다.
-        return f'[{self.pk}]  [{self.title}]'
+        return f'[{self.pk}]  [{self.title}] :: {self.author}'
 
     # 해당 함수는 인터페이스로 이미 제작되어있다.
     # Convention Oever Configuration 이라는, 정해진 이름을 함수로 만들면, 일반적인 기능이 자동적으로 추가.
